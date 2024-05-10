ed67e7f1e4851302e7fac59963fb439820118485
Swapnil Bhosale <bswapnil699@gmail.com>
2024-05-10 20:22:21 +0530

    adding redis for improve the preformance

--- src/main/java/com/example/redisproject/RedisProjectApplication.java
+++ src/main/java/com/example/redisproject/RedisProjectApplication.java
@@ -2,8 +2,10 @@ package com.example.redisproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class RedisProjectApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/redisproject/controller/CarController.java
+++ src/main/java/com/example/redisproject/controller/CarController.java
@@ -2,6 +2,8 @@ package com.example.redisproject.controller;
 
 import com.example.redisproject.model.Car;
 import com.example.redisproject.service.CarService;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,16 +18,19 @@ public class CarController {
     }
 
     @GetMapping("{id}")
+    @Cacheable(value = "car")
     public Car getCar(@PathVariable("id") Long id) {
         return carService.getCarById(id);
     }
 
     @GetMapping("")
+    @Cacheable(value = "car")
     public List<Car> getCars() {
         return carService.getAllCars();
     }
 
     @PostMapping("/create")
+    @CachePut(key = "#car.company", value = "car")
     public Car createCar(@RequestBody Car car) {
         return carService.createCar(car.getCompany(), car.getModel());
     }
